9b5793b598aa250866ef5853f9ab29e6cb4d8c0b
raushan606 <raushankumar606@gmail.com>
2020-03-24 15:31:57 +0530

    Generating authentication tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,28 +10,30 @@ router.post("/users", async (req, res) => {
   
     try {
       await user.save();
-      res.status(201).send(user);
+      const token = user.generateAuthToken()
+      res.status(201).send({user, token});
     } catch (e) {
       res.status(400).send(e);
     }
-  
-    //   user
-    //     .save()
-    //     .then(() => {
-    //       res.status(201).send(user);
-    //       console.log(user);
-    //     })
-    //     .catch(error => {
-    //       res.status(400).send(error);
-    //     });
-    //   console.log(req.body);
+
   });
+
+  router.post('/users/signup', async (req, res) => {
+    try {
+
+    } catch(e) {
+      
+    }
+  })
 
   router.post('/users/login', async (req, res) => {
     try{
 
       const user = await User.findByCredentials(req.body.email, req.body.password)
-      res.send(user)
+
+      const token = await user.generateAuthToken()
+
+      res.send({user, token})
     } catch(e) {
       res.status(400).send(e)
     }
@@ -120,7 +122,7 @@ router.post("/users", async (req, res) => {
   router.delete("/users/:id", async (req, res) => {
     try {
       const user = await User.findByIdAndDelete(req.params.id);
-  
+      
       if (!user) {
         return res.status(404).send();
       }
