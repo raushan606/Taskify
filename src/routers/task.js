61040820fdb090fd93ddc8d2281ec7c67713edb6
raushan606 <raushankumar606@gmail.com>
2020-03-27 01:10:34 +0530

    Sorting data

--- src/routers/task.js
+++ src/routers/task.js
@@ -5,20 +5,32 @@ const auth = require("../middleware/auth");
 
 // ------------------------- !!! --- TASKS --- !!! --------------------------
 // GET /tasks?completed=true
+// GET /tasks?limit=2&skip=3
+// GET /tasks?sortBy=createdAt_asc
 router.get("/tasks", auth, async (req, res) => {
-  const match = {}
+  const match = {};
+  const sort = {};
   if (req.query.completed) {
-    match.completed = req.query.completed === 'true'
+    match.completed = req.query.completed === "true";
   }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
+  
   try {
-    await req.user.populate({
-      path: 'tasks',
-      match,
-      options: {
-        limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
-      }
-    }).execPopulate();
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort
+        }
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
@@ -82,9 +94,12 @@ router.patch("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id", auth,  async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user._id})
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id
+    });
 
     if (!task) {
       res.status(404).send();
