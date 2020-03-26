688776f6f54d9f22b369df95ca66fb5b4de93cf7
raushan606 <raushankumar606@gmail.com>
2020-03-27 00:49:12 +0530

    Filtering Data

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model("Task", {
+const taskSchema = new mongoose.Schema({
     description: {
         required: true,
         type: String,
@@ -15,7 +15,10 @@ const Task = mongoose.model("Task", {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
+},{
+    timestamps: true,
 })
+const Task = mongoose.model("Task", taskSchema)
 
 
 
